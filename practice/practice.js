fa9bbd76c94d7ec371498b31073925ad6171de6b
aemiller2013 <alexandra.e.miller@vanderbilt.edu>
2019-05-09 10:29:45 -0500

    completed task

--- practice/practice.js
+++ practice/practice.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() { 
   // write your javascript code below.
+  for(var i=10;i>0;i--){
+  	console.log(i);
+  }
+  var fruits =["horned melon","avocado","olives"];
+  for(var i=0;i<fruits.length;i++){
+  	console.log(fruits[i]);
+  }
   
-
-
 
 
 
--- script.js
+++ script.js
@@ -1,32 +1,56 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Write your JavaScript below. After you finish a section, comment out the code (command + ?)
   
-  console.log("*********Names, Names, Names***********");
-  ///////////// Names, Names, Names /////////////
-  ///////////////////////////////////////////////
+  // console.log("*********Names, Names, Names***********");
+  // ///////////// Names, Names, Names /////////////
+  // ///////////////////////////////////////////////
+  // var namesNames=["Sam", "Ford", "Karim", "Sarah", "McArthur", "Alex"];
+  // for(var i=0; i<namesNames.length; i++){
+  //   console.log(namesNames[i]);
+  // }
+
+
+
+  // ///////////////////////////////////////////////
+  // ///////////////////////////////////////////////
+
+  // console.log("*********Best Num, Num, Nums***********");
+  // ///////////// Best Num, Num, Nums /////////////
+  // ///////////////////////////////////////////////
+  // var bestNum=[1,7,21,40,31,88];
+  // for(var i=0; i<bestNum.length;i++){
+  //   console.log(bestNum[i]);
+  // }
+  // for(i=0; i<bestNum.length;i++){
+  //   console.log(bestNum[i]+2);
+  // }
+  // for(i=0; i<bestNum.length;i++){
+  //   if(bestNum[i]>10){
+  //     console.log("The number is greater than 10.");
+  //   }else{
+  //     console.log("The number is less than 10.");
+  //   }
+  // }
+  // for(i=0;i<bestNum.length;i++){
+  //   console.log(Math.sqrt(bestNum[i]));
+  // }
 
 
 
 
+  // ///////////////////////////////////////////////
+  // ///////////////////////////////////////////////
 
-  ///////////////////////////////////////////////
-  ///////////////////////////////////////////////
-
-  console.log("*********Best Num, Num, Nums***********");
-  ///////////// Best Num, Num, Nums /////////////
-  ///////////////////////////////////////////////
-
-
-
-
-
-  ///////////////////////////////////////////////
-  ///////////////////////////////////////////////
-
-  console.log("*********Worst, Num, Num, Nums***********");
-  //////////// Worst, Num, Num, Nums  ///////////
-  ///////////////////////////////////////////////
-
+  // console.log("*********Worst, Num, Num, Nums***********");
+  // //////////// Worst, Num, Num, Nums  ///////////
+  // ///////////////////////////////////////////////
+  // var worstNum = [11, 22, 44, 55, 66, 77, 88, 111, 33, 0];
+  // for(var i=0;i<worstNum.length;i++){
+  //   console.log(Math.sqrt(worstNum[i]));
+  // }
+  // for(i=0;i<worstNum.length;i++){
+  //   console.log((worstNum[i]/2)==(Math.round(worstNum[i]/2)));
+  // }
 
 
 
@@ -40,22 +64,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
   //////////////////////////////////////////////////////////////////////////
     // Write `sum` below. 
+    // var myArray=[11,12,13,14,15,16,17,18,19,20];
+    // function sum(myFunctionArray){
+    //   var returnValue=0;
+    //   for(var i=0;i<myFunctionArray.length;i++){
+    //     returnValue += myFunctionArray[i];
+    //   }
+    //   return returnValue;
+    // }
+    // console.log(sum(myArray));
 
 
 
-
-
-    // Uncomment the console.logs below, and look in the console to see if your function worked!
+    // // Uncomment the console.logs below, and look in the console to see if your function worked!
     // console.log(sum([1, 2, 3]))
-    // Expected output: 6
+    // // Expected output: 6
     // console.log(sum([11, 423, 593, 91, 4, 19, 241, -44]))
-    // Expected output: 1338
+    // // Expected output: 1338
 
   //////////////////////////////////////////////////////////////////////////
 
   //////////////////////////////////////////////////////////////////////////
-    // Write `blastOff` below. 
+    // // Write `blastOff` below. 
+    // function blastOff(myNum){
+    //   for(var i=0;i<=myNum;i++){
+    //      if((myNum-i)!=0){
+    //       console.log(myNum-i);
+    //      } else{
+    //       console.log("Blast Off!");
+    //      }
+    //   }
+    // }
 
+    // //test
+    // console.log(blastOff(10));
     
 
 
