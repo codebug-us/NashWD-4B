18a88f0232f22d3602091141c85e774922533b0e
Ford Chope <douglas.f.chope@vanderbilt.edu>
2019-05-09 11:52:29 -0500

    loops

--- practice/practice.js
+++ practice/practice.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() { 
   // write your javascript code below.
   
+for (var i = 10; i>0; i--){
+	console.log(i);
+}
 
+var fruits = ["melon","avocado","olives"];
+
+for (var i = 0;i<fruits.length;i++){
+	console.log(fruits[i]);
+}
 
 
 
--- script.js
+++ script.js
@@ -1,29 +1,67 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Write your JavaScript below. After you finish a section, comment out the code (command + ?)
   
-  console.log("*********Names, Names, Names***********");
+var codebug = ["Ford","McArthur","sarah","sam","alex"];
+
+// for (i=0; i<codebug.length;i++){
+
+//   console.log(codebug[i]);
+// }
+
+// for (i=0; i<codebug.length;i++){
+//   console.log(codebug[i] + "is so smart!");
+// }
+
+  //console.log("*********Names, Names, Names***********");
   ///////////// Names, Names, Names /////////////
   ///////////////////////////////////////////////
+// var num = [7,47,17,27,77,28];
 
+// for (i=0;i<num.length;i++){
+//   console.log(num[i]);
+// }
 
+// var num = [7,47,17,27,77,28];
 
+// for (i=0;i<num.length;i++){
+//   console.log(num[i] + 2);
+// }
 
+//STEP 4
+// var num = [7,47,17,27,77,28];
+
+// for (i=0;i<num.length;i++){
+//   console.log(num[i]);
+// if (num[i]>10){
+//     console.log("more than 10");
+//   } else{
+//     console.log("less than 10");
+//   }
+// }
+
+// var num = [7,47,17,27,77,28];
+
+// for (i=0;i<num.length;i++){
+//   console.log(Math.sqrt(num[i]));
+// }
 
   ///////////////////////////////////////////////
   ///////////////////////////////////////////////
 
-  console.log("*********Best Num, Num, Nums***********");
+  //console.log("*********Best Num, Num, Nums***********");
   ///////////// Best Num, Num, Nums /////////////
   ///////////////////////////////////////////////
 
+// var worstnums = [3,13,34,33,6,15,23,43,2,19];
 
-
-
+// for (i=0;i<worstnums.length;i++){
+//   console.log(worstnums[i]*worstnums[i]);
+// }
 
   ///////////////////////////////////////////////
   ///////////////////////////////////////////////
 
-  console.log("*********Worst, Num, Num, Nums***********");
+  //console.log("*********Worst, Num, Num, Nums***********");
   //////////// Worst, Num, Num, Nums  ///////////
   ///////////////////////////////////////////////
 
@@ -41,8 +79,13 @@ document.addEventListener('DOMContentLoaded', function() {
   //////////////////////////////////////////////////////////////////////////
     // Write `sum` below. 
 
+// var challenge = [3,13,34,33,6,15,23,43,2,19];
 
+// function sum(challenge){
+//   return 
 
+// }
+//   console.log()
 
 
     // Uncomment the console.logs below, and look in the console to see if your function worked!
@@ -56,8 +99,15 @@ document.addEventListener('DOMContentLoaded', function() {
   //////////////////////////////////////////////////////////////////////////
     // Write `blastOff` below. 
 
-    
-
+  function blastOff(num){
+    for (var i=num; i>=0; i--){
+      console.log(i);
+        if (i == 0){
+          console.log("blastOff");
+        }
+    }
+  }
+console.log(blastOff(17));
 
 
     
